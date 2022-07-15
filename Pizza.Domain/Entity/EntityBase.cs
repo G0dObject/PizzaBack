ca92465855e2ef288de26efd60bc5d7ae847c5c1
NIGAsu <vladimirgjob@gmail.com>
2022-07-15 12:50:15 +0300

    Create Repo

---
+++ Pizza.Domain/Entity/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace Pizza.Domain.Entity
+{
+    public abstract class EntityBase
+    {
+        public abstract int Id { get; set; }        
+    }
+}
---
+++ Pizza.Domain/Entity/Food/FoodBase.cs
@@ -0,0 +1,8 @@
+﻿namespace Pizza.Domain.Entity.Food
+{
+    public abstract class FoodBase : EntityBase
+    {
+        public abstract string Name { get; set; }
+        public abstract decimal Price { get; set; }        
+   }
+}
